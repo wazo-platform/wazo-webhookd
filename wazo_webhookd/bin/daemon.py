33b795fac6addc8ee10e9e3c1a78a19cc1312d25
Sebastien Duthil <sduthil@wazo.community>
2017-06-15 14:50:26 -0400

    drop root privileges

--- wazo_webhookd/bin/daemon.py
+++ wazo_webhookd/bin/daemon.py
@@ -4,12 +4,17 @@
 import time
 
 from xivo.daemonize import pidfile_context
+from xivo.user_rights import change_user
 
 FOREGROUND = True  # Always in foreground systemd takes care of daemonizing
 PIDFILE = '/var/run/wazo-webhookd/wazo-webhookd.pid'
+USER = 'wazo-webhookd'
 
 
 def main():
+    if USER:
+        change_user(USER)
+
     with pidfile_context(PIDFILE, FOREGROUND):
         while True:
             time.sleep(1)
