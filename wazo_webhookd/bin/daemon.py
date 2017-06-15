9138c4c2241703c04258907f3ba263c2b014ea9e
Sebastien Duthil <sduthil@wazo.community>
2017-06-15 15:07:28 -0400

    handle SIGTERM
    reason: this is the signal sent by systemd when stopping the
    daemon. If not caught, this signal stops the execution of Python code
    abruptly, no finally are executed

--- wazo_webhookd/bin/daemon.py
+++ wazo_webhookd/bin/daemon.py
@@ -1,6 +1,9 @@
 # Copyright 2017 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0+
 
+import logging
+import signal
+import sys
 import time
 
 from xivo import xivo_logging
@@ -14,12 +17,20 @@ LOGLEVEL = 'debug'
 PIDFILE = '/var/run/wazo-webhookd/wazo-webhookd.pid'
 USER = 'wazo-webhookd'
 
+logger = logging.getLogger(__name__)
+
+
+def _sigterm_handler(signum, frame):
+    logger.info('SIGTERM received, terminating')
+    sys.exit(0)
+
 
 def main():
     if USER:
         change_user(USER)
 
     xivo_logging.setup_logging(LOGFILE, FOREGROUND, DEBUG, LOGLEVEL)
+    signal.signal(signal.SIGTERM, _sigterm_handler)
 
     with pidfile_context(PIDFILE, FOREGROUND):
         while True:
