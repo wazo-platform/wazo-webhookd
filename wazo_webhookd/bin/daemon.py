bedb2c3b3a10ff371358722df8c9e0f550bf7eb1
Sebastien Duthil <sduthil@wazo.community>
2017-06-15 14:56:12 -0400

    add log file

--- wazo_webhookd/bin/daemon.py
+++ wazo_webhookd/bin/daemon.py
@@ -3,10 +3,14 @@
 
 import time
 
+from xivo import xivo_logging
 from xivo.daemonize import pidfile_context
 from xivo.user_rights import change_user
 
+DEBUG = True
 FOREGROUND = True  # Always in foreground systemd takes care of daemonizing
+LOGFILE = '/var/log/wazo-webhookd.log'
+LOGLEVEL = 'debug'
 PIDFILE = '/var/run/wazo-webhookd/wazo-webhookd.pid'
 USER = 'wazo-webhookd'
 
@@ -14,6 +18,8 @@ USER = 'wazo-webhookd'
 def main():
     if USER:
         change_user(USER)
+
+    xivo_logging.setup_logging(LOGFILE, FOREGROUND, DEBUG, LOGLEVEL)
 
     with pidfile_context(PIDFILE, FOREGROUND):
         while True:
