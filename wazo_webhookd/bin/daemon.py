9cffcf7b0c8e96313462d0743e9e2f8dbcb66550
Sebastien Duthil <sduthil@wazo.community>
2017-06-15 14:40:43 -0400

    add pidfile

--- wazo_webhookd/bin/daemon.py
+++ wazo_webhookd/bin/daemon.py
@@ -3,7 +3,13 @@
 
 import time
 
+from xivo.daemonize import pidfile_context
+
+FOREGROUND = True  # Always in foreground systemd takes care of daemonizing
+PIDFILE = '/var/run/wazo-webhookd/wazo-webhookd.pid'
+
 
 def main():
-    while True:
-        time.sleep(1)
+    with pidfile_context(PIDFILE, FOREGROUND):
+        while True:
+            time.sleep(1)
