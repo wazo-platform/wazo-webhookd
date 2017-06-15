29213b6aada8606e002a079367f61ce9e625cb61
Sebastien Duthil <sduthil@wazo.community>
2017-06-15 14:00:23 -0400

    minimal webhookd daemon

---
+++ setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# Copyright 2017 The Wazo Authors  (see the AUTHORS file)
+# SPDX-License-Identifier: GPL-3.0+
+
+from setuptools import setup
+from setuptools import find_packages
+
+
+NAME = 'wazo-webhookd'
+setup(
+    name=NAME,
+    version='1.0',
+    author='Wazo Authors',
+    author_email='dev@wazo.community',
+    url='http://wazo.community',
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': ['{}=wazo_webhookd.bin.daemon:main'.format(NAME)],
+    },
+)
---
+++ wazo_webhookd/bin/daemon.py
@@ -0,0 +1,9 @@
+# Copyright 2017 The Wazo Authors  (see the AUTHORS file)
+# SPDX-License-Identifier: GPL-3.0+
+
+import time
+
+
+def main():
+    while True:
+        time.sleep(1)
