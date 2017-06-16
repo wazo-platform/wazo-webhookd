9ff61ae24d4e2f9a300d9c1bad410d5f420b1c29
Sebastien Duthil <sduthil@wazo.community>
2017-06-16 10:12:13 -0400

    extract main loop to controller

--- wazo_webhookd/bin/daemon.py
+++ wazo_webhookd/bin/daemon.py
@@ -1,25 +1,16 @@
 # Copyright 2017 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0+
 
-import logging
-import signal
 import sys
-import time
 
 from xivo import xivo_logging
 from xivo.daemonize import pidfile_context
 from xivo.user_rights import change_user
+from wazo_webhookd.controller import Controller
 
 from wazo_webhookd import config
 
 FOREGROUND = True  # Always in foreground systemd takes care of daemonizing
-
-logger = logging.getLogger(__name__)
-
-
-def _sigterm_handler(signum, frame):
-    logger.info('SIGTERM received, terminating')
-    sys.exit(0)
 
 
 def main():
@@ -29,8 +20,7 @@ def main():
         change_user(conf['user'])
 
     xivo_logging.setup_logging(conf['log_file'], FOREGROUND, conf['debug'], conf['log_level'])
-    signal.signal(signal.SIGTERM, _sigterm_handler)
 
+    controller = Controller(conf)
     with pidfile_context(conf['pid_file'], FOREGROUND):
-        while True:
-            time.sleep(1)
+        controller.run()
---
+++ wazo_webhookd/controller.py
@@ -0,0 +1,25 @@
+# Copyright 2017 The Wazo Authors  (see the AUTHORS file)
+# SPDX-License-Identifier: GPL-3.0+
+
+import logging
+import time
+import signal
+import sys
+
+logger = logging.getLogger(__name__)
+
+
+class Controller:
+
+    def __init__(self, config):
+        self.config = config
+
+    def run(self):
+        signal.signal(signal.SIGTERM, _sigterm_handler)
+        while True:
+            time.sleep(1)
+
+
+def _sigterm_handler(signum, frame):
+    logger.info('SIGTERM received, terminating')
+    sys.exit(0)
