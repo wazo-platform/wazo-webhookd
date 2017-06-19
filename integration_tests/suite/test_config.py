3ba7bfd009905591011e89d34525bf83fe685ca1
Sebastien Duthil <sduthil@wazo.community>
2017-06-19 14:09:32 -0400

    tests: use webhookd-client

--- integration_tests/suite/test_config.py
+++ integration_tests/suite/test_config.py
@@ -1,10 +1,9 @@
 # Copyright 2017 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0+
 
-import requests
-
 from hamcrest import assert_that
 from hamcrest import has_key
+from wazo_webhookd_client import Client as WebhookdClient
 
 from .test_api.base import BaseIntegrationTest
 from .test_api.base import VALID_TOKEN
@@ -15,8 +14,11 @@ class TestDocumentation(BaseIntegrationTest):
     asset = 'base'
 
     def test_config(self):
-        config_url = 'https://localhost:{port}/1.0/config'.format(port=self.service_port(9300, 'webhookd'))
+        webhookd = self.make_webhookd('localhost', self.service_port(9300, 'webhookd'), VALID_TOKEN)
 
-        result = requests.get(config_url, headers={'X-Auth-Token': VALID_TOKEN}, verify=False).json()
+        result = webhookd.config.get()
 
         assert_that(result, has_key('rest_api'))
+
+    def make_webhookd(self, host, port, token):
+        return WebhookdClient(host, port, token=token, verify_certificate=False)
