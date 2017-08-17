d9e6a8bc9a930e5c013b1e91dc60d75c6e6a2723
Sebastien Duthil <sduthil@wazo.community>
2017-08-17 14:07:55 -0400

    Revert "remove unused import"
    This reverts commit d903d5e13a2780749c41cb1eacd120ac789cc30e.

--- integration_tests/suite/test_http_callback.py
+++ integration_tests/suite/test_http_callback.py
@@ -3,6 +3,7 @@
 
 from mockserver import MockServerClient
 from xivo_test_helpers import until
+from xivo_test_helpers.bus import BusClient
 
 from .test_api.base import BaseIntegrationTest
 from .test_api.fixtures import subscription
