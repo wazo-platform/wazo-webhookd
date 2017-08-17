5ea78fe0f6f605411ce1605af3f33a285c6444aa
Sebastien Duthil <sduthil@wazo.community>
2017-08-17 16:35:42 -0400

    http subscription: add content_type option

--- wazo_webhookd/plugins/subscription/celery_tasks.py
+++ wazo_webhookd/plugins/subscription/celery_tasks.py
@@ -8,15 +8,23 @@ def load(celery_app):
 
     @celery_app.task
     def http_callback(options):
+        headers = {}
+
         body = options.get('body')
         if body:
             body = body.encode('utf-8')
+
         verify = options.get('verify_certificate')
         if verify:
             verify = True if verify == 'true' else verify
             verify = False if verify == 'false' else verify
 
+        content_type = options.get('content_type')
+        if content_type:
+            headers['Content-Type'] = content_type
+
         requests.request(options['method'],
                          options['url'],
                          data=body,
-                         verify=verify)
+                         verify=verify,
+                         headers=headers)
