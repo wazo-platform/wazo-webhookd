3e3ff5e69a0ebc97a00b174a81d74d69d5998c8b
Sebastien Duthil <sduthil@wazo.community>
2017-08-21 15:53:39 -0400

    remove duplicate make_webhookd

--- integration_tests/suite/test_config.py
+++ integration_tests/suite/test_config.py
@@ -3,7 +3,6 @@
 
 from hamcrest import assert_that
 from hamcrest import has_key
-from wazo_webhookd_client import Client as WebhookdClient
 
 from .test_api.base import BaseIntegrationTest
 from .test_api.base import VALID_TOKEN
@@ -14,11 +13,8 @@ class TestConfig(BaseIntegrationTest):
     asset = 'base'
 
     def test_config(self):
-        webhookd = self.make_webhookd('localhost', self.service_port(9300, 'webhookd'), VALID_TOKEN)
+        webhookd = self.make_webhookd(VALID_TOKEN)
 
         result = webhookd.config.get()
 
         assert_that(result, has_key('rest_api'))
-
-    def make_webhookd(self, host, port, token):
-        return WebhookdClient(host, port, token=token, verify_certificate=False)
