01139398cca8c8f75e5455b4afaea858728120bd
Francois Blackburn <fblackburn@wazo.community>
2019-02-13 08:46:34 -0500

    update SPDX-License-Identifier

--- wazo_webhookd/plugins/services/resource.py
+++ wazo_webhookd/plugins/services/resource.py
@@ -1,5 +1,5 @@
 # Copyright 2017 The Wazo Authors  (see the AUTHORS file)
-# SPDX-License-Identifier: GPL-3.0+
+# SPDX-License-Identifier: GPL-3.0-or-later
 
 from wazo_webhookd.rest_api import AuthResource
 from xivo.auth_verifier import required_acl
