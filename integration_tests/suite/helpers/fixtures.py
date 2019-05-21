771dc9df77ab83848433c0436b79ec9bd31e3396
Mehdi Abaakouk <sileht@sileht.net>
2019-05-20 21:15:03 -0400

    tests: add test name into events
    Sometimes tests receive from other tests and fail.
    
    This change add the test name into each events
    to help the debugging.

--- integration_tests/suite/helpers/fixtures.py
+++ integration_tests/suite/helpers/fixtures.py
@@ -1,19 +1,27 @@
 # Copyright 2017-2019 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
+import copy
 from functools import wraps
 from wazo_webhookd_client.exceptions import WebhookdError
 
 from .base import VALID_TOKEN
 
 
-def subscription(subscription_args):
+def subscription(subscription_args, track_test_name=True):
     '''This decorator is only compatible with instance methods, not pure functions.'''
     def decorator(decorated):
         @wraps(decorated)
         def wrapper(self, *args, **kwargs):
             webhookd = self.make_webhookd(VALID_TOKEN)
-            new_subscription = webhookd.subscriptions.create(subscription_args)
+
+            sub = copy.deepcopy(subscription_args)
+            if track_test_name:
+                # Add test name to help debugging
+                sep = "&" if "?" in sub['config']['url'] else "?"
+                sub['config']['url'] += sep + "test_case=" + decorated.__name__
+
+            new_subscription = webhookd.subscriptions.create(sub)
             self.ensure_webhookd_consume_uuid(new_subscription['uuid'])
 
             args = list(args) + [new_subscription]
