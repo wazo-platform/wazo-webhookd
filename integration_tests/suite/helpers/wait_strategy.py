37cb6faa8ee08291aafa312b8d0da6e011a2d6f2
Sebastien Duthil <sduthil@wazo.community>
2021-02-18 09:42:06 -0500

    tests: bump rabbitmq timeout
    Why:
    
    * jenkins takes about 15 seconds to start rabbitmq
    * webhookd takes about 22 seconds to connect to rabbitmq, due to
    exponential backoff

--- integration_tests/suite/helpers/wait_strategy.py
+++ integration_tests/suite/helpers/wait_strategy.py
@@ -1,4 +1,4 @@
-# Copyright 2017-2019 The Wazo Authors  (see the AUTHORS file)
+# Copyright 2017-2021 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 from hamcrest import assert_that, has_entries
@@ -25,4 +25,4 @@ class ConnectedWaitStrategy(WaitStrategy):
                 raise AssertionError('wazo-webhookd is not up yet')
             assert_that(status['bus_consumer'], has_entries({'status': 'ok'}))
 
-        until.assert_(webhookd_is_connected, timeout=20, interval=1)
+        until.assert_(webhookd_is_connected, timeout=30, interval=1)
