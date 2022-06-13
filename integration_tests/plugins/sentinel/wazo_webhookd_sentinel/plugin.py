0b4ae7d3ba39ef8739351935165eecc51bcc23e8
Julien Alie <jalie@wazo.io>
2022-06-13 12:44:56 -0400

    sentinel: modify to use xivo-bus

--- integration_tests/plugins/sentinel/wazo_webhookd_sentinel/plugin.py
+++ integration_tests/plugins/sentinel/wazo_webhookd_sentinel/plugin.py
@@ -1,7 +1,5 @@
-# Copyright 2017-2021 The Wazo Authors  (see the AUTHORS file)
+# Copyright 2017-2022 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0-or-later
-
-import time
 
 from flask_restful import Resource
 
@@ -12,12 +10,21 @@ class SentinelResource(Resource):
     def __init__(self, bus_consumer):
         self._bus_consumer = bus_consumer
 
+    def _get_bindings(self):
+        container = []
+        for binding in self._bus_consumer._ConsumerMixin__queue.bindings:
+            # only grab bindings from subscriptions
+            if 'x-subscription' not in binding.arguments:
+                continue
+
+            headers = binding.arguments.copy()
+            uuid = headers.pop('x-subscription')
+            event = headers.pop('name')
+            container.append({'uuid': uuid, 'event': event, 'headers': headers})
+        return container
+
     def get(self):
-        # wait for changes to be applied
-        while not self._bus_consumer._consumer_updates.empty():
-            time.sleep(0.1)
-        consumers = list(self._bus_consumer._consumers.keys())
-        return {'called': called, 'consumers': consumers}
+        return {'called': called, 'bindings': self._get_bindings()}
 
     def post(self):
         global called
@@ -32,7 +39,7 @@ class BusSentinelResource(Resource):
     _last_event_payload = None
 
     @classmethod
-    def on_message(cls, body, message):
+    def on_message(cls, body):
         if body['name'] == 'webhookd_ping':
             cls._last_event_payload = body['data']['payload']
         elif body['name'] == 'crash_ping':
@@ -50,10 +57,6 @@ class Plugin:
             SentinelResource, '/sentinel', resource_class_args=[bus_consumer]
         )
         api.add_resource(BusSentinelResource, '/sentinel/bus')
-        bus_consumer.subscribe_to_event_names(
-            'sentinel-subscription',
-            ['webhookd_ping', 'crash_ping'],
-            user_uuid=None,
-            wazo_uuid=None,
-            callback=BusSentinelResource.on_message,
-        )
+
+        for event in ['webhookd_ping', 'crash_ping']:
+            bus_consumer.subscribe(event, BusSentinelResource.on_message)
