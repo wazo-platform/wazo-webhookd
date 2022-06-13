1672c153937b26763c36088a7c2b51185e3e26bd
Julien Alie <jalie@wazo.io>
2022-06-13 12:33:18 -0400

    subscription: update bus wrapper
    Why:
    
    * With the changes to bus implementation, we must update the wrapper
    to be compliant

--- wazo_webhookd/plugins/subscription/bus.py
+++ wazo_webhookd/plugins/subscription/bus.py
@@ -1,11 +1,13 @@
-# Copyright 2017-2021 The Wazo Authors  (see the AUTHORS file)
+# Copyright 2017-2022 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
-import functools
 import logging
 import kombu.exceptions
 import uuid
 
+from collections import defaultdict
+from functools import partial
+from wazo_webhookd.auth import get_master_tenant_uuid
 from .celery_tasks import hook_runner_task
 from .schema import subscription_schema
 
@@ -15,6 +17,7 @@ logger = logging.getLogger(__name__)
 class SubscriptionBusEventHandler:
     def __init__(self, bus_consumer, config, service_manager, subscription_service):
         self._bus_consumer = bus_consumer
+        self._subscription_callbacks = defaultdict(list)
         self._config = config
         self._service = subscription_service
         self._service.pubsub.subscribe('created', self.on_subscription_created)
@@ -22,37 +25,56 @@ class SubscriptionBusEventHandler:
         self._service.pubsub.subscribe('deleted', self.on_subscription_deleted)
         self._service_manager = service_manager
 
-    def subscribe(self, bus_consumer):
+    def subscribe(self):
         for subscription in self._service.list():
-            self._add_one_subscription_to_bus(subscription)
+            self._register(subscription)
 
     def on_subscription_created(self, subscription):
-        self._add_one_subscription_to_bus(subscription)
+        self._register(subscription)
 
     def on_subscription_updated(self, subscription):
-        raw_subscription = subscription_schema.dump(subscription)
-        self._bus_consumer.change_subscription(
-            subscription.uuid,
-            subscription.events,
-            subscription.events_user_uuid,
-            subscription.events_wazo_uuid,
-            functools.partial(self._callback, raw_subscription),
-        )
+        self._unregister(subscription)
+        self._register(subscription)
 
     def on_subscription_deleted(self, subscription):
-        self._bus_consumer.unsubscribe_from_event_names(subscription.uuid)
+        self._unregister(subscription)
 
-    def _add_one_subscription_to_bus(self, subscription):
-        raw_subscription = subscription_schema.dump(subscription)
-        self._bus_consumer.subscribe_to_event_names(
-            subscription.uuid,
+    @staticmethod
+    def _build_headers(subscription):
+        headers = {}
+        user_uuid = subscription.events_user_uuid
+        wazo_uuid = subscription.events_wazo_uuid
+        tenant_uuid = subscription.owner_tenant_uuid
+
+        if tenant_uuid != get_master_tenant_uuid():
+            headers.update(tenant_uuid=tenant_uuid)
+        if user_uuid:
+            headers.update({f'user_uuid:{user_uuid}': True})
+        if wazo_uuid:
+            headers.update(origin_uuid=wazo_uuid)
+        return headers
+
+    def _register(self, subscription):
+        uuid = subscription.uuid
+        data = subscription_schema.dump(subscription)
+        extra_headers = self._build_headers(subscription)
+
+        fn, events = self._subscription_callbacks[uuid] = (
+            partial(self._callback, data),
             subscription.events,
-            subscription.events_user_uuid,
-            subscription.events_wazo_uuid,
-            functools.partial(self._callback, raw_subscription),
         )
 
-    def _callback(self, subscription, event, message):
+        for event in events:
+            self._bus_consumer.subscribe(event, fn, headers=extra_headers)
+
+    def _unregister(self, subscription):
+        uuid = subscription.uuid
+        fn, events = self._subscription_callbacks.pop(uuid, (None, []))
+
+        for event in events:
+            self._bus_consumer.unsubscribe(event, fn)
+
+    def _callback(self, subscription, payload):
         try:
             service = self._service_manager[subscription['service']]
         except KeyError:
@@ -70,7 +92,7 @@ class SubscriptionBusEventHandler:
                 str(service.entry_point),
                 self._config.data,
                 subscription,
-                event,
+                payload,
             ).apply_async()
         except kombu.exceptions.OperationalError:
             # NOTE(sileht): That's not perfect in real life, because if celery
@@ -79,10 +101,6 @@ class SubscriptionBusEventHandler:
             # reconnect, we are fine. Otherise we have an exception because of
             # disconnection and our bus will get this message again on
             # reconnection.
-            try:
-                message.requeue()
-            except Exception:
-                logger.error("fail to requeue message")
             raise
         except Exception:
             # NOTE(sileht): We have a programming issue, we don't retry forever
