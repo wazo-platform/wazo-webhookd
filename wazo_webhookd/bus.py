146891c5f18960898a6c3437e39ff61dd4f33e5c
Julien Alie <jalie@wazo.io>
2022-06-20 10:39:53 -0400

    bus: check headers before dispatching handlers
    Why:
    
    * Because multiple handlers can be bound of the same event,
    we must be able to call the correct handler by checking if
    headers are matching
    
    i.e: only dispatch a handler with matching tenant_uuid and/or user_uuid
    and/or origin_uuid

--- wazo_webhookd/bus.py
+++ wazo_webhookd/bus.py
@@ -3,6 +3,7 @@
 
 import logging
 
+from six import iteritems
 from xivo_bus.base import Base
 from xivo_bus.mixins import ThreadableMixin, ConsumerMixin
 
@@ -10,5 +11,39 @@ from xivo_bus.mixins import ThreadableMixin, ConsumerMixin
 logger = logging.getLogger(__name__)
 
 
-class BusConsumer(ThreadableMixin, ConsumerMixin, Base):
+# !!DO NOT RENAME!!
+# Must be the same name to allow remapping of private (mangled) methods
+class _ConsumerMixin(ConsumerMixin):
+    def __check_headers_match(self, headers, binding):
+        # only perform check if exchange type is headers
+        if self.__exchange.type != 'headers':
+            return True
+        compare = all if binding.arguments.get('x-match', 'all') == 'all' else any
+        headers = {k: v for k, v in iteritems(headers) if not k.startswith('x-')}
+        arguments = {
+            k: v for k, v in iteritems(binding.arguments) if not k.startswith('x-')
+        }
+
+        return compare(
+            [k in headers and headers[k] == v for k, v in iteritems(arguments)]
+        )
+
+    def __dispatch(self, event_name, payload, headers=None):
+        with self.__lock:
+            subscriptions = self.__subscriptions[event_name].copy()
+        for (handler, binding) in subscriptions:
+            if not self.__check_headers_match(headers, binding):
+                continue
+            try:
+                handler(payload)
+            except Exception:
+                self.log.exception(
+                    'Handler \'%s\' for event \'%s\' failed',
+                    handler.__name__,
+                    event_name,
+                )
+            continue
+
+
+class BusConsumer(ThreadableMixin, _ConsumerMixin, Base):
     pass
