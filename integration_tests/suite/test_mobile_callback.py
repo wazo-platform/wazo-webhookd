1adcc0f8ebe3452d97e7c7ae421065fc23b6129a
Charles Langlois <charles.langlois@wazo.io>
2024-06-26 15:58:07 -0400

    integration_tests/test_mobile_callback: added test coverage for missed call notifications

--- integration_tests/suite/test_mobile_callback.py
+++ integration_tests/suite/test_mobile_callback.py
@@ -8,14 +8,16 @@ import uuid
 import requests
 from hamcrest import (
     assert_that,
+    contains_exactly,
     contains_inanyorder,
     contains_string,
     equal_to,
     has_entries,
     has_entry,
     has_item,
+    instance_of,
 )
-from mockserver import MockServerClient
+from mockserver import MockServerClient as _MockServerClient
 from wazo_test_helpers import until
 
 from .helpers.base import (
@@ -30,6 +32,13 @@ from .helpers.base import (
 from .helpers.wait_strategy import ConnectedWaitStrategy
 
 SOME_ROUTING_KEY = 'routing-key'
+
+
+class MockServerClient(_MockServerClient):
+    def get_requests(self, path: str):
+        response = self._put('/retrieve?type=requests', {'path': path})
+        response.raise_for_status()
+        return response.json()
 
 
 class BaseMobileCallbackIntegrationTest(BaseIntegrationTest):
@@ -80,6 +89,39 @@ class BaseMobileCallbackIntegrationTest(BaseIntegrationTest):
             },
             headers={'Wazo-Tenant': USERS_TENANT},
         )
+
+    def _given_mobile_subscription(self, user_uuid):
+        # mobile user logs in
+        self.bus.publish(
+            {
+                'name': 'auth_user_external_auth_added',
+                'origin_uuid': 'my-origin-uuid',
+                'data': {'external_auth_name': 'mobile', 'user_uuid': str(user_uuid)},
+            },
+            routing_key=SOME_ROUTING_KEY,
+            headers={
+                'name': 'auth_user_external_auth_added',
+                'origin_uuid': 'my-origin-uuid',
+            },
+        )
+        self._wait_items(
+            functools.partial(self.webhookd.subscriptions.list, recurse=True)
+        )
+        subscriptions = self.webhookd.subscriptions.list(recurse=True)
+        assert (
+            subscription := next(
+                (
+                    s
+                    for s in subscriptions['items']
+                    if s['owner_user_uuid'] == user_uuid and s['service'] == 'mobile'
+                ),
+                None,
+            )
+        )
+
+        assert subscription['events'] and 'user_missed_call' in subscription['events']
+
+        return subscription
 
 
 class TestMobileCallbackFCMProxy(BaseMobileCallbackIntegrationTest):
@@ -277,6 +319,91 @@ class TestMobileCallbackFCMProxy(BaseMobileCallbackIntegrationTest):
 
         self.webhookd.subscriptions.delete(subscription["uuid"])
 
+    def test_missed_call_notification(self):
+        # user logs in
+        subscription = self._given_mobile_subscription(USER_1_UUID)
+
+        self.third_party.mock_any_response(
+            {
+                'httpRequest': {
+                    'path': '/fcm/send',
+                    'body': {
+                        'type': 'JSON',
+                        'json': {
+                            'data': {
+                                'items': json.dumps(
+                                    {
+                                        'caller_id_number': '12221114455',
+                                        'caller_id_name': 'John Doe',
+                                    }
+                                ),
+                                'notification_type': 'missedCall',
+                            }
+                        },
+                        'matchType': 'ONLY_MATCHING_FIELDS',
+                    },
+                },
+                'httpResponse': {
+                    'statusCode': 200,
+                    'body': json.dumps({'message_id': 'message-id-missed-call'}),
+                },
+            }
+        )
+
+        # mobile user misses a call
+        self.bus.publish(
+            {
+                'name': 'user_missed_call',
+                'origin_uuid': 'my-origin-uuid',
+                'data': {
+                    'user_uuid': f'{USER_1_UUID}',
+                    'caller_user_uuid': 'ad5b78cf-6e15-45c7-9ef3-bec36e07e8d6',
+                    'caller_id_name': 'John Doe',
+                    'caller_id_number': '12221114455',
+                    'dialed_extension': '8000',
+                    'conversation_id': '1718908219.36',
+                    'reason': 'no-answer',
+                },
+            },
+            routing_key=SOME_ROUTING_KEY,
+            headers={
+                'name': 'user_missed_call',
+                'origin_uuid': 'my-origin-uuid',
+                'tenant_uuid': USERS_TENANT,
+                f'user_uuid:{USER_1_UUID}': True,
+            },
+        )
+
+        def assert_subscription_logs():
+            sbs = self.webhookd.subscriptions.get_logs(subscription["uuid"])
+            assert sbs and sbs.get('items')
+
+        until.assert_(assert_subscription_logs, timeout=10, interval=0.5)
+
+        # expect FCM API to receive request for push notif
+        self.third_party.verify(
+            request={
+                'path': '/fcm/send',
+                'headers': [
+                    {'name': 'authorization', 'values': [f'key={JWT_TENANT_0}']},
+                ],
+            },
+        )
+
+        logs = self.webhookd.subscriptions.get_logs(subscription["uuid"])
+        assert_that(logs['total'], equal_to(1))
+        assert_that(
+            logs['items'][0],
+            has_entries(
+                status="success",
+                detail=has_entry(
+                    'full_response',
+                    has_entry('topic_message_id', 'message-id-missed-call'),
+                ),
+                attempts=1,
+            ),
+        )
+
 
 class TestMobileCallback(BaseMobileCallbackIntegrationTest):
     asset = 'base'
@@ -284,24 +411,6 @@ class TestMobileCallback(BaseMobileCallbackIntegrationTest):
 
     def setUp(self):
         super().setUp()
-
-        # with open(self.assets_root + "/fake-apple-ca/certs/client.crt") as f:
-        #     ios_apn_certificate = f.read()
-
-        # with open(self.assets_root + "/fake-apple-ca/certs/client.key") as f:
-        #     ios_apn_key = f.read()
-
-        # self.auth.reset_external_auth()
-        # self.auth.set_external_config(
-        #     {
-        #         'mobile': {
-        #             'fcm_api_key': 'FCM_API_KEY',
-        #             'ios_apn_certificate': ios_apn_certificate,
-        #             'ios_apn_private': ios_apn_key,
-        #             'is_sandbox': False,
-        #         }
-        #     }
-        # )
 
 
 class TestMobileCallbackFCMLegacy(TestMobileCallback):
@@ -484,6 +593,88 @@ class TestMobileCallbackFCMLegacy(TestMobileCallback):
 
         self.webhookd.subscriptions.delete(subscription["uuid"])
 
+    def test_missed_call_notification(self):
+        # user logs in
+        subscription = self._given_mobile_subscription(USER_1_UUID)
+
+        self.third_party.mock_any_response(
+            {
+                'httpRequest': {
+                    'path': '/fcm/send',
+                    'body': {
+                        'type': 'JSON',
+                        'json': {
+                            'data': {
+                                'items': {
+                                    'caller_id_number': '12221114455',
+                                    'caller_id_name': 'John Doe',
+                                },
+                                'notification_type': 'missedCall',
+                            }
+                        },
+                        'matchType': 'ONLY_MATCHING_FIELDS',
+                    },
+                },
+                'httpResponse': {
+                    'statusCode': 200,
+                    'body': json.dumps({'message_id': 'message-id-missed-call'}),
+                },
+            }
+        )
+
+        # mobile user misses a call
+        self.bus.publish(
+            {
+                'name': 'user_missed_call',
+                'origin_uuid': 'my-origin-uuid',
+                'data': {
+                    'user_uuid': f'{USER_1_UUID}',
+                    'caller_user_uuid': 'ad5b78cf-6e15-45c7-9ef3-bec36e07e8d6',
+                    'caller_id_name': 'John Doe',
+                    'caller_id_number': '12221114455',
+                    'dialed_extension': '8000',
+                    'conversation_id': '1718908219.36',
+                    'reason': 'no-answer',
+                },
+            },
+            routing_key=SOME_ROUTING_KEY,
+            headers={
+                'name': 'user_missed_call',
+                'origin_uuid': 'my-origin-uuid',
+                'tenant_uuid': USERS_TENANT,
+                f'user_uuid:{USER_1_UUID}': True,
+            },
+        )
+
+        def assert_subscription_logs():
+            assert self.webhookd.subscriptions.get_logs(subscription["uuid"])
+
+        until.assert_(assert_subscription_logs, timeout=10, interval=0.5)
+
+        # expect FCM API to receive request for push notif
+        self.third_party.verify(
+            request={
+                'path': '/fcm/send',
+                'headers': [
+                    {'name': 'authorization', 'values': ['key=FCM_API_KEY']},
+                ],
+            },
+        )
+
+        logs = self.webhookd.subscriptions.get_logs(subscription["uuid"])
+        assert_that(logs['total'], equal_to(1))
+        assert_that(
+            logs['items'][0],
+            has_entries(
+                status="success",
+                detail=has_entry(
+                    'full_response',
+                    has_entry('topic_message_id', 'message-id-missed-call'),
+                ),
+                attempts=1,
+            ),
+        )
+
 
 class TestMobileCallbackFCMv1(TestMobileCallback):
     def setUp(self):
@@ -505,7 +696,7 @@ class TestMobileCallbackFCMv1(TestMobileCallback):
             "universe_domain": "googleapis.com",
         }
 
-        fake_token = {
+        self.oauth2_token = {
             "access_token": "valid-access-token",
             "scope": "",
             "token_type": "Bearer",
@@ -533,7 +724,7 @@ class TestMobileCallbackFCMv1(TestMobileCallback):
                 },
                 'httpResponse': {
                     'statusCode': 200,
-                    'body': json.dumps(fake_token),
+                    'body': json.dumps(self.oauth2_token),
                 },
             }
         )
@@ -707,6 +898,141 @@ class TestMobileCallbackFCMv1(TestMobileCallback):
         )
 
         self.webhookd.subscriptions.delete(subscription["uuid"])
+
+    def test_missed_call_notification(self):
+        # user logs in
+        subscription = self._given_mobile_subscription(USER_1_UUID)
+
+        self.fcm_third_party.mock_any_response(
+            {
+                'httpRequest': {
+                    'path': '/v1/projects/project-123/messages:send',
+                    'headers': {
+                        'Content-Type': ['application/json'],
+                        'Authorization': [
+                            f'Bearer {self.oauth2_token["access_token"]}'
+                        ],
+                    },
+                },
+                'httpResponse': {
+                    'statusCode': 200,
+                    'body': json.dumps({'name': 'message-id-missed-call'}),
+                },
+            }
+        )
+
+        # mobile user misses a call
+        self.bus.publish(
+            {
+                'name': 'user_missed_call',
+                'origin_uuid': 'my-origin-uuid',
+                'data': {
+                    'user_uuid': f'{USER_1_UUID}',
+                    'caller_user_uuid': 'ad5b78cf-6e15-45c7-9ef3-bec36e07e8d6',
+                    'caller_id_name': 'John Doe',
+                    'caller_id_number': '12221114455',
+                    'dialed_extension': '8000',
+                    'conversation_id': '1718908219.36',
+                    'reason': 'no-answer',
+                },
+            },
+            routing_key=SOME_ROUTING_KEY,
+            headers={
+                'name': 'user_missed_call',
+                'origin_uuid': 'my-origin-uuid',
+                'tenant_uuid': USERS_TENANT,
+                f'user_uuid:{USER_1_UUID}': True,
+            },
+        )
+
+        def assert_subscription_logs():
+            assert self.webhookd.subscriptions.get_logs(subscription["uuid"])
+
+        until.assert_(assert_subscription_logs, timeout=10, interval=0.5)
+
+        # expect FCM API to receive request for push notif
+        def assert_fcm_request():
+            try:
+                self.fcm_third_party.verify(
+                    request={
+                        'path': '/v1/projects/project-123/messages:send',
+                        'headers': [
+                            {
+                                'name': 'authorization',
+                                'values': [
+                                    f'Bearer {self.oauth2_token["access_token"]}'
+                                ],
+                            },
+                        ],
+                        'body': {
+                            'type': 'STRING',
+                            'contentType': 'application/json',
+                        },
+                    }
+                )
+            except Exception:
+                raise AssertionError
+
+        until.assert_(assert_fcm_request, timeout=10, interval=0.5)
+
+        fcm_notification_requests = self.fcm_third_party.get_requests(
+            path='/v1/projects/project-123/messages:send'
+        )
+        assert fcm_notification_requests and len(fcm_notification_requests) == 1
+        request = fcm_notification_requests[0]
+
+        # expecting a request with a json body and proper authorization header
+        assert_that(
+            request,
+            has_entries(
+                body=has_entries(
+                    type='STRING',
+                    string=instance_of(str),
+                    contentType='application/json',
+                ),
+                headers=has_entries(
+                    'Content-Type',
+                    contains_exactly('application/json'),
+                    'Authorization',
+                    contains_exactly(f'Bearer {self.oauth2_token["access_token"]}'),
+                ),
+            ),
+        )
+
+        # expecting the json body to contain the proper payload
+        request_body_json = json.loads(request['body']['string'])
+
+        assert_that(
+            request_body_json,
+            has_entries(
+                message=has_entries(
+                    android=has_entries(
+                        notification=has_entries(
+                            title=instance_of(str),
+                            body=instance_of(str),
+                        )
+                    ),
+                    data=has_entries(
+                        items=instance_of(str),
+                        notification_type='missedCall',
+                    ),
+                ),
+            ),
+        )
+
+        logs = self.webhookd.subscriptions.get_logs(subscription["uuid"])
+        assert_that(logs['total'], equal_to(1))
+        assert_that(
+            logs['items'][0],
+            has_entries(
+                status="success",
+                detail=has_entry(
+                    'full_response',
+                    has_entry('topic_message_id', 'message-id-missed-call'),
+                ),
+                attempts=1,
+            ),
+        )
 
 
 class TestMobileCallbackAPNS(TestMobileCallback):
@@ -1044,3 +1370,87 @@ class TestMobileCallbackAPNS(TestMobileCallback):
         )
 
         self.webhookd.subscriptions.delete(subscription["uuid"])
+
+    def test_missed_call_notification(self):
+        self.auth.set_external_auth(
+            {
+                'token': 'token-android',
+                'apns_token': 'token-ios',
+                'apns_notification_token': 'apns-notification-token',
+            }
+        )
+        # user logs in
+        subscription = self._given_mobile_subscription(USER_1_UUID)
+
+        message_uuid = uuid.uuid4()
+        self.apns_third_party.mock_simple_response(
+            path='/3/device/apns-notification-token',
+            responseBody={'tracker': f'tracker-missed-call-{message_uuid}'},
+            statusCode=200,
+        )
+
+        # mobile user misses a call
+        self.bus.publish(
+            {
+                'name': 'user_missed_call',
+                'origin_uuid': 'my-origin-uuid',
+                'data': {
+                    'user_uuid': f'{USER_1_UUID}',
+                    'caller_user_uuid': 'ad5b78cf-6e15-45c7-9ef3-bec36e07e8d6',
+                    'caller_id_name': 'A Mctest',
+                    'caller_id_number': '8001',
+                    'dialed_extension': '8000',
+                    'conversation_id': '1718908219.36',
+                    'reason': 'no-answer',
+                },
+            },
+            routing_key=SOME_ROUTING_KEY,
+            headers={
+                'name': 'user_missed_call',
+                'origin_uuid': 'my-origin-uuid',
+                'tenant_uuid': USERS_TENANT,
+                f'user_uuid:{USER_1_UUID}': True,
+            },
+        )
+
+        def assert_subscription_logs():
+            assert self.webhookd.subscriptions.get_logs(subscription["uuid"])
+
+        until.assert_(assert_subscription_logs, timeout=10, interval=0.5)
+
+        def assert_apns_notification_posted():
+            try:
+                self.apns_third_party.verify(
+                    request={
+                        'path': '/3/device/apns-notification-token',
+                        'headers': [
+                            {
+                                'name': 'authorization',
+                                'values': [f'Bearer {JWT_TENANT_0}'],
+                            },
+                            {'name': 'user-agent', 'values': ['wazo-webhookd']},
+                        ],
+                    },
+                )
+            except Exception:
+                raise AssertionError()
+
+        until.assert_(assert_apns_notification_posted, timeout=10, interval=0.5)
+
+        logs = self.webhookd.subscriptions.get_logs(subscription["uuid"])
+        assert_that(logs['total'], equal_to(1))
+        print(logs['items'][0])
+        assert_that(
+            logs['items'][0],
+            has_entries(
+                status="success",
+                detail=has_entry(
+                    'full_response',
+                    has_entry(
+                        'response_body',
+                        has_entry('tracker', f'tracker-missed-call-{message_uuid}'),
+                    ),
+                ),
+                attempts=1,
+            ),
+        )
