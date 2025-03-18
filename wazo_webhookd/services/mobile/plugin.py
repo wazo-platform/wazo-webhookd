d24ae849f2b61236be3fbc77c4f07bf207dbeba2
Charles Langlois <charles.langlois@wazo.io>
2025-03-18 15:22:23 -0400

    services.mobile: APNS data-only messageReceived

--- wazo_webhookd/services/mobile/plugin.py
+++ wazo_webhookd/services/mobile/plugin.py
@@ -81,7 +81,7 @@ ApsContentDict = TypedDict(
     {
         'badge': int,
         'sound': str,
-        'content-available': str,
+        'content-available': int,
         'alert': dict[str, str],
     },
 )
@@ -553,7 +553,7 @@ class PushNotification:
         data_only: bool,
     ):
         headers, payload, token = self._create_apn_message(
-            message_title, message_body, data
+            message_title, message_body, data, data_only
         )
 
         use_sandbox = self.external_config.get('use_sandbox', False)
@@ -598,6 +598,7 @@ class PushNotification:
         message_title: str | None,
         message_body: str | None,
         data: NotificationPayload,
+        data_only: bool,
     ):
         apns_call_topic = self.config['mobile_apns_call_topic']
         apns_default_topic = self.config['mobile_apns_default_topic']
@@ -644,7 +645,9 @@ class PushNotification:
                 },
             )
 
-            if message_title or message_body:
+            if data_only:
+                payload['aps']['content-available'] = 1
+            elif message_title or message_body:
                 alert = {}
                 if message_title:
                     alert['title'] = message_title
