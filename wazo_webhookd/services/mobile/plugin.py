6de424d16825a0a128443a587bf0d577c3c8c1e6
Charles Langlois <charles.langlois@wazo.io>
2025-09-24 09:13:49 -0400

    pyupgrade 3.9 -> 3.11

--- wazo_webhookd/services/mobile/plugin.py
+++ wazo_webhookd/services/mobile/plugin.py
@@ -126,9 +126,7 @@ REQUEST_TIMEOUTS = httpx.Timeout(connect=10, read=15, write=15, pool=None)
 DEFAULT_ANDROID_CHANNEL_ID = 'io.wazo.songbird'
 
 
-class NotificationType(
-    StrEnum
-):  # TODO: StrEnum would be better, when we have Python 3.11
+class NotificationType(StrEnum):
     MESSAGE_RECEIVED = 'messageReceived'
     VOICEMAIL_RECEIVED = 'voicemailReceived'
     INCOMING_CALL = 'incomingCall'
