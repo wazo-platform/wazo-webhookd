26d38be3e10fa5e1287d37b2db5e5efa860d39ca
Charles Langlois <charles.langlois@wazo.io>
2025-10-24 12:14:21 -0400

    mobile: update integration tests
    for apns notifications
    which must include title and body even for data-only

--- integration_tests/suite/test_mobile_callback.py
+++ integration_tests/suite/test_mobile_callback.py
@@ -2155,6 +2155,28 @@ class TestMobileCallbackAPNS(TestMobileCallback):
             ),
         )
 
+        with self.last_apns_request(token='apns-notification-token') as request:
+            assert 'aps' in request and 'alert' in request['aps']
+            assert_that(
+                request['aps']['alert'],
+                has_entries(
+                    title='New Message from sender-name',
+                    body='chat content',
+                ),
+            )
+
+            assert_that(
+                request,
+                has_entries(
+                    notification_type='messageReceived',
+                    items=has_entries(
+                        alias='sender-name',
+                        content='chat content',
+                        notification_timestamp=an_iso_timestamp(),
+                    ),
+                ),
+            )
+
         self.webhookd.subscriptions.delete(subscription["uuid"])
 
     def test_global_voicemail_message_created_notification(self):
@@ -2357,3 +2379,26 @@ class TestMobileCallbackAPNS(TestMobileCallback):
                 attempts=1,
             ),
         )
+
+        with self.last_apns_request(token='apns-notification-token') as request:
+            assert 'aps' in request and 'alert' in request['aps']
+            assert_that(
+                request['aps']['alert'],
+                has_entries(
+                    title='Missed Call',
+                    body='From: A Mctest (8001)',
+                ),
+            )
+            assert_that(
+                request,
+                has_entries(
+                    notification_type='missedCall',
+                    items=has_entries(
+                        caller_id_name='A Mctest',
+                        caller_id_number='8001',
+                        notification_timestamp=an_iso_timestamp(),
+                    ),
+                ),
+            )
+
+        self.webhookd.subscriptions.delete(subscription["uuid"])
