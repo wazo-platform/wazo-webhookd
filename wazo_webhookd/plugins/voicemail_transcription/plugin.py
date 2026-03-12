a422840df5ad2737eb771f2b80af435f80f2b618
Remy Bienvenu <remy.bienvenu@wazo.io>
2026-03-12 07:29:10 -0700

    transcription: listen for voicemail bus events
    GEN-101

--- wazo_webhookd/plugins/voicemail_transcription/plugin.py
+++ wazo_webhookd/plugins/voicemail_transcription/plugin.py
@@ -3,11 +3,19 @@
 
 from __future__ import annotations
 
-from typing import TYPE_CHECKING
+import logging
+from typing import TYPE_CHECKING, Any
+
+from wazo_bus.resources.voicemail.event import (
+    GlobalVoicemailMessageCreatedEvent,
+    UserVoicemailMessageCreatedEvent,
+)
 
 if TYPE_CHECKING:
     from wazo_webhookd.bus import BusConsumer
     from wazo_webhookd.types import PluginDependencyDict
+
+logger = logging.getLogger(__name__)
 
 
 class Plugin:
@@ -15,3 +23,17 @@ class Plugin:
 
     def load(self, dependencies: PluginDependencyDict) -> None:
         self._bus_consumer = dependencies['bus_consumer']
+        self._bus_consumer.subscribe(
+            UserVoicemailMessageCreatedEvent.name,
+            self._on_user_voicemail_created,
+        )
+        self._bus_consumer.subscribe(
+            GlobalVoicemailMessageCreatedEvent.name,
+            self._on_global_voicemail_created,
+        )
+
+    def _on_user_voicemail_created(self, event: dict[str, Any]) -> None:
+        logger.debug('Received user voicemail created event: %s', event)
+
+    def _on_global_voicemail_created(self, event: dict[str, Any]) -> None:
+        logger.debug('Received global voicemail created event: %s', event)
--- wazo_webhookd/plugins/voicemail_transcription/tests/test_plugin.py
+++ wazo_webhookd/plugins/voicemail_transcription/tests/test_plugin.py
@@ -23,3 +23,28 @@ class TestPlugin:
         plugin.load(dependencies)  # type: ignore[arg-type]
 
         assert plugin._bus_consumer is bus_consumer
+
+    def test_load_subscribes_to_voicemail_events(self) -> None:
+        plugin = Plugin()
+        bus_consumer = Mock()
+        dependencies: dict = {
+            'api': Mock(),
+            'auth_client': Mock(),
+            'bus_consumer': bus_consumer,
+            'bus_publisher': Mock(),
+            'config': Mock(),
+            'service_manager': Mock(),
+            'next_token_change_subscribe': Mock(),
+        }
+
+        plugin.load(dependencies)  # type: ignore[arg-type]
+
+        bus_consumer.subscribe.assert_any_call(
+            'user_voicemail_message_created',
+            plugin._on_user_voicemail_created,
+        )
+        bus_consumer.subscribe.assert_any_call(
+            'global_voicemail_message_created',
+            plugin._on_global_voicemail_created,
+        )
+        assert bus_consumer.subscribe.call_count == 2
