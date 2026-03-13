3efd044179cebd128bd2f8ea55947eb8d473c52b
Remy Bienvenu <remy.bienvenu@wazo.io>
2026-03-13 08:24:48 -0700

    transcript: Send event on transcript ready
    GEN-101

---
+++ wazo_webhookd/plugins/voicemail_transcription/events.py
@@ -0,0 +1,11 @@
+# Copyright 2026 The Wazo Authors  (see the AUTHORS file)
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+from __future__ import annotations
+
+from wazo_bus.resources.common.event import ServiceEvent
+
+
+class VoicemailTranscriptionFinishedEvent(ServiceEvent):
+    name = 'voicemail_transcription_finished'
+    routing_key_fmt = 'voicemail.transcription.finished'
